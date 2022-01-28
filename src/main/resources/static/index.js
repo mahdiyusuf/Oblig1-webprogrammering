dd52d972319bf391d81459aa0eeb8fa8ccbf3d0e
s364517 <89968022+s364517@users.noreply.github.com>
2022-01-28 15:12:44 +0100

    c

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -6,6 +6,8 @@ let string = ""
 let film = ""
 let valgt;
 let handlekurv = document.getElementById("handlekurv")
+let bilett = "";
+let biletter = [];
 
 for(let movie of movielist){
     string += " <option>" + movie + "</option> "
@@ -33,19 +35,28 @@ if (movielist[moviechooses] === "2012"){
     }
 }
 
-let input1 = document.getElementById("antall")
-let input2 = document.getElementById("fornavn")
-let input3 = document.getElementById("etternavn")
-let input4 = document.getElementById("telefonnr")
-let input5 = document.getElementById("email")
-
-let t1 = document.getElementById("t1")
-let t2 = document.getElementById("t2")
-let t3 = document.getElementById("t3")
-let t4 = document.getElementById("t4")
-let t5 = document.getElementById("t5")
-
 function Kjopbillett() {
+    let input0 = document.getElementById("handlekurv")
+    let input1 = document.getElementById("Antall")
+    let input2 = document.getElementById("Fornavn")
+    let input3 = document.getElementById("Etternavn")
+    let input4 = document.getElementById("Telefonnr")
+    let input5 = document.getElementById("Email")
+
+    let t1 = document.getElementById("t1")
+    let t2 = document.getElementById("t2")
+    let t3 = document.getElementById("t3")
+    let t4 = document.getElementById("t4")
+    let t5 = document.getElementById("t5")
+
+    t1.innerHTML = "";
+    t2.innerHTML = "";
+    t3.innerHTML = "";
+    t4.innerHTML = "";
+    t5.innerHTML = "";
+
+
+    let film = input0.value
     let antall = input1.value
     let fornavn = input2.value
     let etternavn = input3.value
@@ -53,30 +64,62 @@ function Kjopbillett() {
     let email = input5.value
 
 
-    if (antall !== "") {
-        valgt = true
-        t1.innerHTML = "Kjopbillett"
-    } else {
-        t1.innerHTML = "Må skrive inn noe antall"
+    let tiltak = true
+
+    if (film === "Velg film") {
+        tiltak = false
     }
-    if (fornavn !== "") {
-        valgt = true
-    } else {
-        t2.innerHTML = ""
+    if (antall === ""){
+        tiltak = false
+        t1.innerHTML = " Må skrive noe inn i antall  "
+    }
+    if (fornavn === ""){
+        tiltak = false
+        t2.innerHTML = " Må skrive noe inn i fornavn "
+    }
+    if (etternavn === ""){
+        tiltak = false
+        t3.innerHTML = " Må skrive noe inn i etternavn "
+    }
+    if (telefonnr === ""){
+        tiltak = false
+        t4.innerHTML = " Må skrive noe inn i telefonnummer "
+    }
+    if (email === ""){
+        tiltak = false
+        t5.innerHTML = " Må skrive noe inn i email "
     }
 
-    if (etternavn !== "") {
-        valgt = true
-    }else{
-        t3.innerHTML = ""
-    }
-    if (telefonnr !== "") {
-        valgt = true
-    }
-    if (email !== "") {
-        valgt = true
+    if(tiltak === false){
+        return;
     }
 
+    bilett = {
+        film: film,
+        antall: antall,
+        fornavn: fornavn,
+        etternavn: etternavn,
+        telefonnr:telefonnr,
+        email: email,
+
+
+    }
+    document.getElementById("Billettene" ).innerHTML = "<tr><th>Film</th>"
+    + "<th>Antall</th>"
+    + "<th>Fornavn</th>"
+    + "<th>Etternavn</th>"
+    + "<th>Telefonnr</th>"
+    + "<th>Email</th></tr>"
+
+    biletter.push(bilett)
+    for (let i = 0; i < biletter.length; i++) {
+        document.getElementById("Billettene").innerHTML += "<tr><td>" + biletter[i].film + "</td> " +
+            "<td>" + biletter[i].antall + "</td> " +
+            "<td>" + biletter[i].fornavn + "</td> " +
+            "<td>" + biletter[i].etternavn + "</td> " +
+            "<td>" + biletter[i].telefonnr + "</td> " +
+            "<td>" + biletter[i].email + "</td> </tr>"
+    }
 }
 
 
